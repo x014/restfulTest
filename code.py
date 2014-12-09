4486e77f9b7893526c0498a62f1756bef0e0b077
whl <hellohano@gmail.com>
2014-12-09 18:16:45 +0800

    1.完善了CRUD操作 2.完善了以JSON格式返回字符串

--- code.py
+++ code.py
@@ -3,33 +3,53 @@ __author__ = 'Administrator'
 
 import web
 import sqlite3
+import json
 
 render = web.template.render('templates/')
 urls = (
-    '/(.*)', 'index'
+    '/user', 'index',
+    '/user/(.*)', 'index'
 )
 
 
 class index:
 
-    def GET(self, name):
-        user = select_user("hano")
-        print "it is a get request about ", user
-        return render.index(name)
-    def POST(self, name):
-        insert_user(name)
+    def GET(self, *name):
+        user_info = web.input()
+        user = select_user(user_info.name)
+        print "it is a get request about ", name
+        web.header('content-type', 'text/json')
+        return json.dumps(user)
+
+    def POST(self, *name):  # post参数必须跟请求数对应
         print "it is a post request about "
-        return render.index(name)
-    def PUT(self, name):
-        insert_user(name)
+        user_info = web.input()
+        age = ""
+        try:
+            age = user_info.age
+        except AttributeError, e:
+            pass
+        rowcount = insert_user(user_info.name, age)
+        return rowcount
+
+    def PUT(self):
+        user_info = web.input()
+        insert_user(user_info.name, user_info.age)
         print "it is a put request"
-        return render.index(name)
-    def PATCH(self, name):
+        return 0
+
+    def PATCH(self):
         print "it is a patch request"
-        return render.index(name)
-    def DELETE(self, name):
+        user_data = web.input()
+        print user_data
+        update_user(user_data.name, user_data.age)
+        return 0
+
+    def DELETE(self):
         print "it is a delete request"
-        return render.index(name)
+        user_data = web.input()
+        delete_user(user_data.name)
+        return 0
 
 
 def init_user():
@@ -37,21 +57,22 @@ def init_user():
     cursor = sqlite_conn.cursor()
     sql_del="DROP TABLE IF EXISTS user;"
     cursor.execute(sql_del)
-    sql_create_table = "create table user (id integer primary key,pid integer,name varchar(10) UNIQUE,age integer)"
+    sql_create_table = "create table user (id integer primary key,pid integer,name varchar(10) ,age integer)"
     cursor.execute(sql_create_table)
     sqlite_conn.commit()
     cursor.close()
     sqlite_conn.close()
 
 
-def insert_user(name):
+def insert_user(name, age=0):
     sqliteConn = sqlite3.connect("test.db")
     cursor = sqliteConn.cursor()
-    sql_insert = "insert into user (name) values (\'" + name + "\');"
-    cursor.execute(sql_insert)
+    sql_insert = "insert into user (name, age) values (?, ?);"
+    rowcount = cursor.execute(sql_insert, (name, age))
     sqliteConn.commit()
     cursor.close()
     sqliteConn.close()
+    return rowcount
 
 
 def select_user(name):
@@ -66,13 +87,27 @@ def select_user(name):
     return users
 
 
-def update_user(age):
-    #"update catalog set name='Boy' where id = 0"
+def update_user(name, age):
+    sqliteConn = sqlite3.connect("test.db")
+    cursor = sqliteConn.cursor()
+    update_sql = "update user set age=? where name=?"
+    cursor.execute(update_sql, (age, name))
+    sqliteConn.commit()
+    cursor.close()
+    sqliteConn.close()
 
+
+def delete_user(name):
+    sqliteConn = sqlite3.connect("test.db")
+    cursor = sqliteConn.cursor()
+    sql_delete = "delete from user where name=?"
+    cursor.execute(sql_delete, (name,))
+    sqliteConn.commit()
+    cursor.close()
+    sqliteConn.close()
 
 
 if __name__ == "__main__":
     init_user()
-    insert_user("Hano233")
     app = web.application(urls, globals())
     app.run()
